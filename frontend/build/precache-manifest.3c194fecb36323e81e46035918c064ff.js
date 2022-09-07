54259d9170dd18aeb174acff9a92944c8fa96497
nidarg <gradin_g@yahoo.com>
2022-09-07 11:27:42 +0300

    gg

---
+++ frontend/build/precache-manifest.3c194fecb36323e81e46035918c064ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a184f4e1c943da19819",
+    "url": "/static/css/main.d942db01.chunk.css"
+  },
+  {
+    "revision": "0a184f4e1c943da19819",
+    "url": "/static/js/main.0a184f4e.chunk.js"
+  },
+  {
+    "revision": "82193c4130ac966b1105",
+    "url": "/static/js/1.82193c41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f799e5f02b84497a6451c7afed1f9775",
+    "url": "/static/media/spinner.f799e5f0.gif"
+  },
+  {
+    "revision": "ba1d19cd48af99dc740dc28dd5b0ff3d",
+    "url": "/static/media/landing-photo-2.ba1d19cd.jpg"
+  },
+  {
+    "revision": "b9394d561fcae64dc0e075a77654e4f1",
+    "url": "/index.html"
+  }
+];
